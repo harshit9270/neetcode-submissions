082411bb62e7ced9109f16aa75528664db68ce3c
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2026-09-07 08:30:21 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,13 @@
+// Brute Force - Sort both strings
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s==t)
+            return true;
+        return false;
+    }
+};
