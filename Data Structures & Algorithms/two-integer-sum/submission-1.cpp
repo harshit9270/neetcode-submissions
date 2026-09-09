fe805553428841da97485e2a473070b0efe32749
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2026-09-09 19:43:18 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,19 @@
+// Using hash map (2 Pass)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i=0; i<nums.size(); i++)
+            mp[nums[i]] = i;
+        
+        for(int i=0; i<nums.size(); i++){
+            int diff = target-nums[i];
+            if(mp.count(diff) and mp[diff] != i)
+                return {i, mp[diff]};
+        }
+
+        return {};
+    }
+};
